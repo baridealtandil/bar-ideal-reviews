acaf53f030951483b07cf954fae67bc85dcd40f0
baridealtandil <baridealtandil@gmail.com>
2026-06-25 02:01:17 -0300

    Add debug endpoint

---
+++ api/debug.js
@@ -0,0 +1,12 @@
+const MAKE_API_TOKEN = 'c941991c-0e0a-4e15-80fe-8425868afe57';
+const MAKE_ZONE = 'us2.make.com';
+
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  try {
+    const makeHeaders = { 'Authorization': 'Token ' + MAKE_API_TOKEN, 'Content-Type': 'application/json' };
+    const r = await fetch('https://' + MAKE_ZONE + '/api/v2/data-stores/111536/data/token', { headers: makeHeaders });
+    const d = await r.json();
+    res.status(200).json({ makeStatus: r.status, raw: d, topKeys: Object.keys(d), record: d.record, dataKeys: d.record?.data ? Object.keys(d.record.data) : [] });
+  } catch(e) { res.status(500).json({ error: e.message }); }
+}
